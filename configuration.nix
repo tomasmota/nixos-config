bf87876e39b5bec11f75950985ad3c2772e53830
tomasmota <tomasrebelomota@gmail.com>
2023-12-20 21:25:25 +0100

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,31 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    [ ./hardware-configuration.nix ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev"; # GRUB 2 installs to the EFI partition.
-    efiSupport = true;
-    copyKernels = true; # Ensures that the kernel and initrd are copied to the EFI partition.
-    default = "nixos";
-    extraEntries = ''
-      menuentry "Windows" {
-        search --fs-uuid --no-floppy --set=root B7AC-9D7F
-        chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-      }
-    '';
+  boot.loader = {
+    timeout = 5;
+    # Define the EFI system partition mount point.
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      device = "nodev"; # GRUB 2 installs to the EFI partition.
+      efiSupport = true;
+      copyKernels = true; # Ensures that the kernel and initrd are copied to the EFI partition.
+      default = "nixos";
+      extraEntries = ''
+        menuentry "Windows" {
+          search --fs-uuid --no-floppy --set=root B7AC-9D7F
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+    };
   };
 
-  boot.loader.timeout = 5;
-  # Define the EFI system partition mount point.
-  boot.loader.efi.canTouchEfiVariables = true;
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/B7AC-9DF7";
     fsType = "vfat";
@@ -40,12 +36,8 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_DK.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "da_DK.UTF-8";
     LC_IDENTIFICATION = "da_DK.UTF-8";
@@ -71,9 +63,6 @@
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -83,17 +72,10 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   programs.zsh.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tomas = {
     isNormalUser = true;
     description = "tomas";
@@ -101,7 +83,6 @@
     shell = pkgs.zsh;
     packages = with pkgs; [
       firefox
-      kate
       grub2
       neovim
       gparted
@@ -116,23 +97,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     #neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     #gparted
-  #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;

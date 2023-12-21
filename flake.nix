f7b454b0043348ed27bc26c73ab688984b18339f
tomasmota <tomasrebelomota@gmail.com>
2023-12-21 15:10:26 +0100

    add home-manager

--- flake.nix
+++ flake.nix
@@ -2,20 +2,29 @@
   description = "NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { nixpkgs, home-manager, ... }: 
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
-	desktop = lib.nixosSystem {
-	  inherit system;
-	  modules = [ ./configuration.nix ];
+        desktop = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+
+      homeConfigurations."tomas" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home-manager/home.nix ];
       };
     };
-  };
 }
